c45696a717438cce57aafe4f153b4fd94b3d137d
ricardfiguls <ricard.figuls@gmail.com>
2017-11-10 19:13:54 +0100

    setting window options

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const {app, BrowserWindow} = require('electron') 
-const url = require('url') 
-const path = require('path')  
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
 
-let win  
+let win
 
-function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600}) 
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
-      protocol: 'file:', 
-      slashes: true 
-   })) 
-}  
+function createWindow() {
+    win = new BrowserWindow({
+        minWidth: 500,
+        minHeight: 350,
+        width: 800,
+        height: 600,
+        'autoHideMenuBar': true
+    })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+}
 
-app.on('ready', createWindow) 
+app.on('ready', createWindow)
